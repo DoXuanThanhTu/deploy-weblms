c92d514af3cd511eebb62e999082d9502a8af614
DoXuanThanhTu <166835284+DoXuanThanhTu@users.noreply.github.com>
2025-05-09 20:06:33 +0700

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -44,7 +44,9 @@ const registerUser = async (req, res) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV == "production",
-    maxAge: 24 * 60 * 60 * 1000,
+        sameSite: "None",
+
+    maxAge: 2 * 60 * 60 * 1000,
   });
   return res.status(200).json(user);
 };
